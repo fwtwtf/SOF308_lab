10549b85564c6acae2f3580c3b406f3c9b086575
thanh <fwtwtf2005@gmail.com>
2024-11-03 21:04:54 +0700

    lab thay binh

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+
+//import bai1 from './components/lab1/bai1.vue'
+//import bai2 from './components/lab1/bai2.vue'
+//import bai3 from './components/lab1/bai3.vue'
+import bai4 from './components/lab1/bai4.vue'
+
+//createApp(bai1).mount('#app')
+//createApp(bai2).mount('#app')
+//createApp(bai3).mount('#app')
+createApp(bai4).mount('#app')
